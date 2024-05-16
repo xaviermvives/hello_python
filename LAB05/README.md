88cb2b35c1cd557de5b29dc223d0ca8e7eefcba5
xavier <montjuic79@gmail.com>
2024-05-16 14:21:58 +0200

    LAB05 added

---
+++ LAB05/README.md
@@ -0,0 +1,7 @@
+# CATEGORIZING VALUES
+
+- Use numeric, string, list data tipes
+- Use a for loop
+- Use the print() function
+
+A mixed-type list
---
+++ LAB05/mixed_type_list.py
@@ -0,0 +1,9 @@
+# you can mix data types in a Python list
+
+myMixedTypeList= [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+
+# using a for statement to traverse the list and print the data type
+
+for item in myMixedTypeList:
+    print("{} is of the data type {}".format(item, type(item)))
+    
