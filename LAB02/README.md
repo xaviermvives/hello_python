88239134ad56634428f2411e8e6fc01bb9ead6b5
xavier <montjuic79@gmail.com>
2024-05-16 11:49:13 +0200

    LAB02 added

---
+++ LAB02/README.md
@@ -0,0 +1,14 @@
+## Working with **Numeric Data Types**
+# The 'int', 'float' and 'complex' data types
+# built-in function 'type()'
+# built-in function 'str()' 
+
+## **Bool** data type
+# True or False
+
+
+## **Function**
+## a **function** is a piece of reusable code with a name
+
+## Library
+# a collection of functions is called a **library**
---
+++ LAB02/data_types.py
@@ -0,0 +1,42 @@
+print("Python has three numeric types: int, float, and complex") # muestra por pantalla una cadena texto
+
+# creating a variable:
+myValue = 1
+# printamos la variable
+print(myValue)
+# mostramos el tipo de la variable:
+print(type(myValue)) # <class 'int'>
+# concatenamos numeros y texto con str() y +:
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+# float type:
+myValue= 3.14
+# printamos la variable
+print(myValue) # <class 'float'>
+# mostramos el tipo de la variable:
+print(type(myValue))
+# concatenamos numeros y texto con str() y +:
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+
+# complex data type
+myValue=5j
+# printamos la variable
+print(myValue)
+# mostramos el tipo de la variable:
+print(type(myValue)) # <class 'complex'>
+# concatenamos numeros y texto con str() y +:
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+
+# bool data type
+myValue=True
+# printamos la variable
+print(myValue)
+# mostramos el tipo de la variable:
+print(type(myValue)) # <class 'bool'>
+# concatenamos numeros y texto con str() y +:
+print(str(myValue) + " is of the data type " + str(type(myValue)))
