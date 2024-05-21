650329f44098d8cd8d32fa089518e09c1b6918e1
xavier <montjuic79@gmail.com>
2024-05-21 09:52:54 +0200

    LAB07 conditional1.py completed with and else

--- LAB07/conditional1.py
+++ LAB07/conditional1.py
@@ -4,5 +4,6 @@ userReply = input("Do you need to ship a package? (Enter yes or no) ")
 # cheking the affirmative response and printing a message
 if userReply == "yes": # == ->equal to
     print("We can help you ship that package!")
+else: # if user enters 'no' the program prints another message
+    print("Please come back when you need to ship a package. Thank you.")
 
-# if user enters 'no' the program exits
